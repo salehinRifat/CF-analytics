47137066a3b6cacf5d8cbc9b3d7a4b48f28db292
Abu Salehin Rifat <salehinrifat2@gmail.com>
2024-06-18 11:37:26 +0600

    Fetched data from codeforces api

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,18 @@
 
+import { useEffect, useState } from "react";
+
 
 const Home = () => {
+    const [contest, setContest] = useState([]);
+    useEffect(() => {
+        fetch('https://codeforces.com/api/contest.list')
+            .then(res => res.json())
+            .then(data => {
+                const before = data.result.filter(item => item.phase === 'BEFORE');
+                setContest(before);
+            })
+    }, [])
+    console.log(contest);
     return (
         <div>
             <h1>Hello World</h1>
